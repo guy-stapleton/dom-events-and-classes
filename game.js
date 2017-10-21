0fb5fa29e0a0e2077bd47fa2a00499173a4d683c
Guy Stapleton <guy.stapleton+1@gmail.com>
2017-10-21 18:16:14 +1300

    Created score function

--- game.js
+++ game.js
@@ -44,7 +44,18 @@ function updateCounts () {
   }
 
   // WRITE CODE HERE TO COUNT BLUE, GREEN, AND INVISIBLE DOTS
-
+  var markers = document.getElementsByClassName('board')[0].children;
+  for (var i = 0; i < markers.length; i++) {
+    if(markers[i].classList.contains('blue')) {
+      totals.blue += 1;
+    }
+    if(markers[i].classList.contains('green')){
+      totals.green += 1;
+    }
+    if (markers[i].classList.contains('invisible')) {
+      totals.invisible += 1;
+    }
+  }
   // Once you've done the counting, this function will update the display
   displayTotals(totals)
 }
